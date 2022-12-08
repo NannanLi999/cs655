579a6201266f4f2429063e53988ed9497b7785fb
NannanLi999 <44977884+NannanLi999@users.noreply.github.com>
2022-12-08 13:06:04 -0500

    Create script.sh

---
+++ web/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+bash Anaconda3-2022.10-Linux-x86_64.sh
+source ~/.bashrc
+pip install flask
+python inferface.py
