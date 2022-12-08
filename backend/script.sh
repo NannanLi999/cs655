c6f423480f01519f91cd497a1716de67fd3d1c2b
NannanLi999 <44977884+NannanLi999@users.noreply.github.com>
2022-12-08 12:52:03 -0500

    Create script.sh

---
+++ backend/script.sh
@@ -0,0 +1,7 @@
+wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+bash Anaconda3-2022.10-Linux-x86_64.sh
+source ~/.bashrc
+pip install flask
+pip install nltk
+wget https://github.com/NannanLi999/cs655/blob/main/backend/app.py
+python app.py
